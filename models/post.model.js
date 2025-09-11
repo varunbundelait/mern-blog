01dd511be869a04a4dc315a0c67de1acfe46cda7
varun singh <backsup34@gmail.com>
2025-09-11 11:31:57 +0530

    s10

---
+++ models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2019/02/28/09/15/message-4025602_1280.jpg",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
